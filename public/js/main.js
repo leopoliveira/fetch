7374b92e44b2cfa9d190bdec7a6ef85de5bdbe3f
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2021-11-01 19:54:43 -0300

    confirmar em master

--- public/js/main.js
+++ public/js/main.js
@@ -3,22 +3,96 @@ const consultar = document.querySelector("#btn-consulta");
 const respFetch = document.querySelector("#respFetch");
 const URL_CONSULTA = "https://jsonplaceholder.typicode.com/posts"; //Maiúsculo por ser uma variável de configuração
 
-function fetchConsulta() {
-    fetch(URL_CONSULTA)
-        .then((response) => {
-            response.json()
-            .then((data) => {
-                let html = "";
+// ---USANDO PROMISSES---
+
+// function fetchConsulta() {
+//     fetch(URL_CONSULTA)
+//         .then((response) => {
+//             respFetch.innerHTML = "Carregando...";
+//             response.json()
+//             .then((data) => {
+//                 let html = "";
+//                 html += "<ul>";
+
+//                 for(let i in data) {
+//                     html += `<li>
+//                         <div class="container">
+//                             <div class="avatar">${data[i].userId}</div>
+//                             <div class="postBody" style="text-transform: capitalize;">
+//                                 <h1 style="text-transform: capitalize;">${data[i].title}</h1>
+//                                 <p>${data[i].body}</p>
+//                                 <span>Question ID: ${data[i].id}</span>
+//                             </div>
+//                         </div>
+//                     </li>`
+//                 };
+
+//                 html += "</ul>";
+//                 respFetch.innerHTML = html;
+//             })
+//             .catch((err) => {
+//                 respFetch.innerHTML = `Deu erro na hora de transformar a string JSON em Objeto e o erro foi: ${err}`;
+//             });
+//         })
+//         .catch((err) => {
+//             respFetch.innerHTML = `Deu erro na hora de requisitar os dados e o erro foi: ${err}`;
+//         });
+// };
+
+// ---USANDO ASYNC AWAIT---
+
+// async function fetchConsulta() {
+//     let resposta = await fetch(URL_CONSULTA);
+//     let json = await resposta.json();
+
+//     let html = "";
+//                 html += "<ul>";
+
+//                 for(let i in json) {
+//                     html += `<li>
+//                         <div class="container">
+//                             <div class="avatar">${json[i].userId}</div>
+//                             <div class="postBody" style="text-transform: capitalize;">
+//                                 <h1 style="text-transform: capitalize;">${json[i].title}</h1>
+//                                 <p>${json[i].body}</p>
+//                                 <span>Question ID: ${json[i].id}</span>
+//                             </div>
+//                         </div>
+//                     </li>`
+//                 };
+
+//                 html += "</ul>";
+//                 respFetch.innerHTML = html;
+
+// };
+
+//---USANDO XMLHTTPREQUEST---
+
+async function fetchConsulta() {
+
+    let req = new XMLHttpRequest();
+    let json;
+
+    req.open("GET", URL_CONSULTA);
+
+    req.send();
+
+    req.onreadystatechange = () => {
+        if(req.readyState == 4 && req.status == 200) {
+            json = JSON.parse(req.response);
+        }
+
+        let html = "";
                 html += "<ul>";
 
-                for(let i in data) {
+                for(let i in json) {
                     html += `<li>
                         <div class="container">
-                            <div class="avatar">${data[i].userId}</div>
+                            <div class="avatar">${json[i].userId}</div>
                             <div class="postBody" style="text-transform: capitalize;">
-                                <h1 style="text-transform: capitalize;">${data[i].title}</h1>
-                                <p>${data[i].body}</p>
-                                <span>Question ID: ${data[i].id}</span>
+                                <h1 style="text-transform: capitalize;">${json[i].title}</h1>
+                                <p>${json[i].body}</p>
+                                <span>Question ID: ${json[i].id}</span>
                             </div>
                         </div>
                     </li>`
@@ -26,15 +100,8 @@ function fetchConsulta() {
 
                 html += "</ul>";
                 respFetch.innerHTML = html;
-            })
-            .catch((err) => {
-                respFetch.innerHTML = `Deu erro na hora de transformar a string JSON em Objeto e o erro foi: ${err}`;
-            });
-        })
-        .catch((err) => {
-            respFetch.innerHTML = `Deu erro na hora de requisitar os dados e o erro foi: ${err}`;
-        });
-};
+}
+    };
 
 
 consultar.addEventListener("click", fetchConsulta);
